7df4b17c527a959e1c1e39b4ade87c9daeecb638
Olia Gerasimova <olga.gerasimova92@yandex.ru>
2025-11-01 17:44:35 +0300

    remove incorrect tests with spy object

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,25 +1,14 @@
 import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FelineTest {
-    @Spy
-    private Feline felineSpy;
-
-    @Test
-    public void getKittensWithoutParamInvokesGetKittensWith1() {
-        felineSpy.getKittens();
-        verify(felineSpy, times(1)).getKittens(1);
-
-    }
 
     @Test
     public void getKittensWithoutParamReturnsDefaultOne() {
@@ -33,12 +22,6 @@ public class FelineTest {
         Feline feline = new Feline();
         List<String> actualFoodList = feline.eatMeat();
         assertEquals(List.of("Животные", "Птицы", "Рыба"), actualFoodList);
-    }
-
-    @Test
-    public void eatMeatInvokesGetFoodWithParamPredator() throws Exception {
-        felineSpy.eatMeat();
-        verify(felineSpy, times(1)).getFood("Хищник");
     }
 
     @Test
